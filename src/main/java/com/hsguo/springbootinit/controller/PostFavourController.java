d4478302780bf7457a979ab558bd942dca4466e1
HanshengGUO <hsguo@aol.com>
2023-05-21 16:54:07 +0200

    Bug fixed in the template

---
+++ src/main/java/com/hsguo/springbootinit/controller/PostFavourController.java
@@ -0,0 +1,110 @@
+package com.hsguo.springbootinit.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hsguo.springbootinit.common.BaseResponse;
+import com.hsguo.springbootinit.common.ErrorCode;
+import com.hsguo.springbootinit.common.ResultUtils;
+import com.hsguo.springbootinit.exception.BusinessException;
+import com.hsguo.springbootinit.exception.ThrowUtils;
+import com.hsguo.springbootinit.model.dto.post.PostQueryRequest;
+import com.hsguo.springbootinit.model.dto.postfavour.PostFavourAddRequest;
+import com.hsguo.springbootinit.model.dto.postfavour.PostFavourQueryRequest;
+import com.hsguo.springbootinit.model.entity.Post;
+import com.hsguo.springbootinit.model.entity.User;
+import com.hsguo.springbootinit.model.vo.PostVO;
+import com.hsguo.springbootinit.service.PostFavourService;
+import com.hsguo.springbootinit.service.PostService;
+import com.hsguo.springbootinit.service.UserService;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子收藏接口
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@RestController
+@RequestMapping("/post_favour")
+@Slf4j
+public class PostFavourController {
+
+    @Resource
+    private PostFavourService postFavourService;
+
+    @Resource
+    private PostService postService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 收藏 / 取消收藏
+     *
+     * @param postFavourAddRequest
+     * @param request
+     * @return resultNum 收藏变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doPostFavour(@RequestBody PostFavourAddRequest postFavourAddRequest,
+                                              HttpServletRequest request) {
+        if (postFavourAddRequest == null || postFavourAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能操作
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postFavourAddRequest.getPostId();
+        int result = postFavourService.doPostFavour(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+    /**
+     * 获取我收藏的帖子列表
+     *
+     * @param postQueryRequest
+     * @param request
+     */
+    @PostMapping("/my/list/page")
+    public BaseResponse<Page<PostVO>> listMyFavourPostByPage(@RequestBody PostQueryRequest postQueryRequest,
+                                                             HttpServletRequest request) {
+        if (postQueryRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        User loginUser = userService.getLoginUser(request);
+        long current = postQueryRequest.getCurrent();
+        long size = postQueryRequest.getPageSize();
+        // 限制爬虫
+        ThrowUtils.throwIf(size > 20, ErrorCode.PARAMS_ERROR);
+        Page<Post> postPage = postFavourService.listFavourPostByPage(new Page<>(current, size),
+                postService.getQueryWrapper(postQueryRequest), loginUser.getId());
+        return ResultUtils.success(postService.getPostVOPage(postPage, request));
+    }
+
+    /**
+     * 获取用户收藏的帖子列表
+     *
+     * @param postFavourQueryRequest
+     * @param request
+     */
+    @PostMapping("/list/page")
+    public BaseResponse<Page<PostVO>> listFavourPostByPage(@RequestBody PostFavourQueryRequest postFavourQueryRequest,
+            HttpServletRequest request) {
+        if (postFavourQueryRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        long current = postFavourQueryRequest.getCurrent();
+        long size = postFavourQueryRequest.getPageSize();
+        Long userId = postFavourQueryRequest.getUserId();
+        // 限制爬虫
+        ThrowUtils.throwIf(size > 20 || userId == null, ErrorCode.PARAMS_ERROR);
+        Page<Post> postPage = postFavourService.listFavourPostByPage(new Page<>(current, size),
+                postService.getQueryWrapper(postFavourQueryRequest.getPostQueryRequest()), userId);
+        return ResultUtils.success(postService.getPostVOPage(postPage, request));
+    }
+}
